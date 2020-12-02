81f57f5392f386591fd92a5dc3e477b60ff2f971
Roland Szabo <roland.szabo753@gmail.com>
2020-12-02 09:39:46 +0100

    day 2 second part

--- 2/2.py
+++ 2/2.py
@@ -1,7 +1,7 @@
 import pdb
 
 
-class PasswordEntry(object):
+class SRPPasswordEntry(object):
     def __init__(self, min_c, max_c, char, password):
         self.min_c = min_c
         self.max_c = max_c
@@ -30,21 +30,60 @@ class PasswordEntry(object):
         return cls(min_c, max_c, char, password)
 
     def __repr__(self):
-        return 'PasswordEntry(min_c={}, max_c={}, char={}, password={})'.format(
+        return 'SRPPasswordEntry(min_c={}, max_c={}, char={}, password={})'.format(
             self.min_c, self.max_c, self.char, self.password
         )
 
 
-def parse_input():
+class OTCASPasswordEntry(object):
+    def __init__(self, first, second, char, password):
+        self.first = first
+        self.second = second
+        self.char = char
+        self.password = password
+
+    def is_complying(self):
+        return (self.password[self.first - 1] == self.char) != (self.password[self.second - 1] == self.char)
+
+    @classmethod
+    def parse(cls, s):
+        parts = s.split(' ')
+        range_c = parts[0].split('-')
+
+        first = int(range_c[0])
+        second = int(range_c[1])
+
+        char = parts[1][:-1]
+
+        password = parts[2]
+
+        return cls(first, second, char, password)
+
+    def __repr__(self):
+        return 'OTCASPasswordEntry(first={}, second={}, char={}, password={})'.format(
+            self.first, self.second, self.char, self.password
+        )
+
+
+def parse_input(cls):
     with open('input', 'r') as f:
         lines = f.readlines()
-        return [PasswordEntry.parse(line) for line in lines]
+        return [cls.parse(line) for line in lines]
+
+
+def count_complying(cls):
+    db = parse_input(cls)
+    return len(filter(lambda e: e.is_complying(), db))
 
 
 def first():
-    db = parse_input()
-    print(len(filter(lambda e: e.is_complying(), db)))
+    print(count_complying(SRPPasswordEntry))
+
+
+def second():
+    print(count_complying(OTCASPasswordEntry))
 
 
 if __name__ == '__main__':
     first()
+    second()
