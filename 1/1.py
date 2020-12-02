b6c8f015dfa68c94a8bcb01e59dd91f562d9baa3
Roland Szabo <roland.szabo753@gmail.com>
2020-12-02 08:26:29 +0100

    first day second part

--- 1/1.py
+++ 1/1.py
@@ -1,5 +1,5 @@
 
-def main():
+def first():
     expense_sheet = []
     with open('input', 'r') as f:
         expense_sheet = [int(i) for i in f.readlines()]
@@ -11,5 +11,20 @@ def main():
                     print(expense_sheet[i] * expense_sheet[j])
                     return
 
+def second():
+    expense_sheet = []
+    with open('input', 'r') as f:
+        expense_sheet = [int(i) for i in f.readlines()]
+
+    for i in range(len(expense_sheet)):
+        for j in range(len(expense_sheet)):
+            for k in range(len(expense_sheet)):
+                if i != j and j != k and i != k:
+                    if expense_sheet[i] + expense_sheet[j] + expense_sheet[k] == 2020:
+                        print(expense_sheet[i] * expense_sheet[j] * expense_sheet[k])
+                        return
+
+
 if __name__ == '__main__':
-    main()
+    first()
+    second()
