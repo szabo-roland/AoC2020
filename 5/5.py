64a637738fab7c55b335893425e0fd532899fbbb
Roland Szabo <roland.szabo753@gmail.com>
2020-12-05 11:26:49 +0100

    day 5 first

---
+++ 5/5.py
@@ -0,0 +1,18 @@
+def parse_ticket(ticket):
+    return int(ticket.replace('F', '0').replace('B', '1').replace('R', '1').replace('L', '0'), 2)
+
+def parse_input():
+    with open('input', 'r') as f:
+        return [parse_ticket(ticket.strip()) for ticket in f.readlines()]
+
+def first(tickets):
+    return tickets[-1]
+
+
+def main():
+    tickets = parse_input()
+    tickets.sort()
+    print(first(tickets))
+
+if __name__ == '__main__':
+    main()
