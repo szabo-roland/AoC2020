eb979261f34fd5e875e0c9b052e4607116504e44
Roland Szabo <roland.szabo753@gmail.com>
2020-12-10 13:47:05 +0100

    day 10 second

--- 10/10.py
+++ 10/10.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from itertools import combinations
 
 
 def parse_input():
@@ -9,7 +10,7 @@ def parse_input():
 def preprocess(data):
     result = sorted(data)
     result.append(result[-1] + 3)
-    return result
+    return [0] + result
 
 
 def diff_reducer(acc, num):
@@ -19,7 +20,7 @@ def diff_reducer(acc, num):
 
 
 def get_diffs(data):
-    return reduce(diff_reducer, data, {'diffs': [], 'last': 0})['diffs']
+    return reduce(diff_reducer, data[1:], {'diffs': [], 'last': data[0]})['diffs']
 
 
 def first(data):
@@ -28,6 +29,59 @@ def first(data):
     threes = len(filter(lambda e: e == 3, diffs))
     return ones * threes
 
+
+def is_valid(data):
+    return all([d <= 3 for d in get_diffs(data)])
+
+
+def get_all_middle_combinations(middle):
+    result = []
+    for i in range(len(middle) + 1):
+        result.extend([list(m) for m in combinations(middle, i)])
+
+    return result
+
+
+def count_valid_subsequences(data):
+    if len(data) < 3:
+        return 1
+
+    first = data[0]
+    last = data[-1]
+    middles = get_all_middle_combinations(data[1:-1])
+    valids = 0
+    for middle in middles:
+        if is_valid([first] + middle + [last]):
+            valids += 1
+
+    return valids
+
+
+def get_partitions(data):
+    result = []
+    temp = []
+    prev = 0
+    for i in data:
+        if i - prev < 3:
+            temp.append(i)
+        else:
+            result.append(temp)
+            temp = [i]
+        prev = i
+
+    return result
+
+
+def second(data):
+    result = 1
+    partitions = get_partitions(data)
+    for partition in partitions:
+        result *= count_valid_subsequences(partition)
+
+    return result
+
+
 if __name__ == '__main__':
     data = preprocess(parse_input())
     print(first(data))
+    print(second(data))
